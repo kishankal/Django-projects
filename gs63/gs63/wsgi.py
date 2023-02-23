3c924f1abc064810e7391d6591978c509f5ce30a
kishankal <kkalariya200@gmail.com>
2023-02-23 16:28:45 +0530

    adding_login_logout_profile_authemtication_complete_project

--- gs63/gs63/wsgi.py
+++ gs63/gs63/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for crudproject1 project.
+WSGI config for gs63 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudproject1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gs63.settings')
 
 application = get_wsgi_application()
