8b70cd7d282462ba2e59efe4e4b1d9881090d115
kiahankal <kkalariya200@gmail.com>
2023-02-24 12:43:01 +0530

    adding_signup_Login_logout_Authenticateuser_and_manymore

---
+++ Login_logout_Authenticateuser_passwordchange_and_manymore/enroll/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render,HttpResponseRedirect
+from .forms import SignupForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm,SetPasswordForm
+from django.contrib.auth import authenticate,login,logout,update_session_auth_hash
+
+# Create your views here.
+
+# Signup function
+def sign_up(request):
+    if request.method == 'POST':
+        fm = SignupForm(request.POST)
+        if fm.is_valid():
+            messages.success(request,'Account Created Successfully!!!')
+            fm.save()
+            
+    else:
+        fm = SignupForm()
+    return render(request, 'enroll/signup.html',{'form':fm})
+
+# login view function
+def user_login(request):
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            fm = AuthenticationForm(request=request,data=request.POST)
+            if fm.is_valid():
+                uname = fm.cleaned_data['username']
+                upass = fm.cleaned_data['password']
+                user = authenticate(username=uname,password=upass)
+                if user is not None:
+                    login(request,user)
+                    messages.success(request,'Logged in successfully !!')
+                    return HttpResponseRedirect('/profile/')
+                # fm = AuthenticationForm()
+        else:
+            fm = AuthenticationForm()
+        return render(request,'enroll/userlogin.html',{'form':fm})
+    else:
+        return HttpResponseRedirect('/profile/')
+
+# Profile Function
+def user_profile(request):
+    if request.user.is_authenticated:
+        return render(request,'enroll/profile.html',{'name':request.user})
+    else:
+        return HttpResponseRedirect('/login/')
+
+# logout function
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/login/')  
+
+# change password with old password
+def user_change_pass(request):
+    if request.user.is_authenticated:
+
+        if request.method == "POST":
+            fm = PasswordChangeForm(user=request.user,data=request.POST)
+            if fm.is_valid():
+                fm.save()
+                update_session_auth_hash(request,fm.user)
+                return HttpResponseRedirect('/profile/')
+
+        fm = PasswordChangeForm(user=request.user)
+        return render(request,'enroll/changepass.html',{'form':fm})
+    else:
+        messages.error(request,'First you have to Login')
+        return HttpResponseRedirect('/login/')
+    
+# change password without old password
+def user_set_pass(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            fm = SetPasswordForm(user=request.user, data=request.POST)
+            if fm.is_valid():
+                fm.save()
+                update_session_auth_hash(request.fm.user)
+                return HttpResponseRedirect('/profile/')
+            
+        fm = SetPasswordForm(user=request.user)
+        return render(request,'enroll/changepassword1.html',{'form':fm})
+    else:
+        messages.error(request,'First You have to Login')
+        return HttpResponseRedirect('/login/')
